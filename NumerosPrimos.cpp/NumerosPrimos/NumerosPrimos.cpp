32e1322e9b0001924217c3f1dc2507d226df4a86
BraulioMercado <43398760+BraulioMercado@users.noreply.github.com>
2020-02-04 13:31:08 -0800

    Add files via upload

---
+++ NumerosPrimos.cpp/NumerosPrimos/NumerosPrimos.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main() 
+{
+	int divisor = 1, divisores = 0, num = 0;
+	cout << "Ingrese numero: ";
+	cin >> num;
+	do {
+		if (num % divisor == 0) 
+		{
+			divisores++;
+		}
+		divisor++;
+	} 
+	while (divisor <= num);
+
+	if (divisores == 2) {
+		cout << "n-> El numero es PRIMO.";
+	}
+	else {
+		cout << "n-> El numero NO es PRIMO.";
+	}
+	return 0;
+}
